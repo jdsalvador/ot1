df739d09ac9bfc2400ffbfd3eb2db4ce5df0ae31
unknown <juandomingosalvador@gmail.com>
2018-02-18 12:39:58 -0300

    make it better

---
+++ routes/todo.server.route.js
@@ -0,0 +1,18 @@
+// ./express-server/routes/todo.server.route.js
+import express from 'express';
+
+//import controller file
+import * as todoController from '../controllers/todo.server.controller';
+import * as otController from '../controllers/ot.server.controller';
+
+// get an instance of express router
+const router = express.Router();
+
+router.route('/')
+     .get(otController.getModelOts);
+
+router.route('/:id')
+      .get(otController.getModelOt);
+
+
+export default router;
